8f885c20f3850d9e8d2d8a0dfdacb2cd93556530
Majeur <majeur.android@gmail.com>
2016-01-10 15:11:47 +0100

    Added About dialog in sample app

--- app/src/main/java/com/majeur/preferencekit/sample/SettingsActivity.java
+++ app/src/main/java/com/majeur/preferencekit/sample/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.majeur.preferencekit.sample;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,8 +11,8 @@ import android.widget.Toast;
 
 import com.majeur.preferencekit.CheckBoxPreference;
 import com.majeur.preferencekit.DialogPreference;
-import com.majeur.preferencekit.Preference;
 import com.majeur.preferencekit.ExtraButtonsPreference;
+import com.majeur.preferencekit.Preference;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -38,7 +39,7 @@ public class SettingsActivity extends AppCompatActivity {
             buttonsPreference.setOnExtraButtonClickListener(new ExtraButtonsPreference.OnExtraButtonClickListener() {
                 @Override
                 public void onExtraButtonClick(int which) {
-                    Toast.makeText(getActivity(), "Clicked " + which, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Button clicked: " + which, Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -46,29 +47,41 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
             super.onCreateOptionsMenu(menu, inflater);
-            menu.add(0, 0, 0, "Lock");
+            inflater.inflate(R.menu.main, menu);
         }
 
-        private boolean aBoolean = true;
+        private boolean mPreferencesLocked = true;
 
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
-            if (item.getItemId() == 0) {
-                for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
-                    android.preference.Preference p = getPreferenceScreen().getPreference(i);
+            switch (item.getItemId()) {
+                case R.id.action_lock:
+                    for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+                        android.preference.Preference p = getPreferenceScreen().getPreference(i);
 
-                    if (p instanceof Preference)
-                        ((Preference) p).setLocked(aBoolean);
-                    else if (p instanceof DialogPreference)
-                        ((DialogPreference) p).setLocked(aBoolean);
-                    else if (p instanceof CheckBoxPreference)
-                        ((CheckBoxPreference) p).setLocked(aBoolean);
-                }
-                aBoolean = !aBoolean;
-                return true;
+                        if (p instanceof Preference)
+                            ((Preference) p).setLocked(mPreferencesLocked);
+                        else if (p instanceof DialogPreference)
+                            ((DialogPreference) p).setLocked(mPreferencesLocked);
+                        else if (p instanceof CheckBoxPreference)
+                            ((CheckBoxPreference) p).setLocked(mPreferencesLocked);
+                    }
+                    mPreferencesLocked = !mPreferencesLocked;
+                    return true;
+
+                case R.id.action_about:
+                    showAboutDialog();
+                    return true;
             }
-
             return super.onOptionsItemSelected(item);
+        }
+
+        private void showAboutDialog() {
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("About")
+                    .setView(getActivity().getLayoutInflater().inflate(R.layout.about_dialog_message, null))
+                    .setNegativeButton(android.R.string.ok, null)
+                    .show();
         }
     }
 }
